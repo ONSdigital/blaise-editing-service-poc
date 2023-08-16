10372bbc5d48d3344328fe02009660c821d04b08
willst1 <thomas.wills@ons.gov.uk>
2023-08-16 15:06:00 +0100

    Changed Factsheet content view

--- src/client/components/FactsheetContent.tsx
+++ src/client/components/FactsheetContent.tsx
@@ -6,24 +6,35 @@ interface FactsheetContentProps {
 
 export default function FactsheetContent({ factsheet }: FactsheetContentProps) {
   return (
-    <div data-testid="factsheetId">
-      {factsheet.CaseId}
-      {factsheet.Address.AddressLine1}
-      {factsheet.Address.AddressLine2}
-      {factsheet.Address.AddressLine3}
-      {factsheet.Address.AddressLine4}
-      {factsheet.Address.County}
-      {factsheet.Address.Postcode}
-      {factsheet.Address.Town}
-      {factsheet.InterviewerName}
-      {factsheet.NumberOfRespondents}
-      {factsheet.OutcomeCode}
+
+    <dl
+      className="ons-metadata ons-metadata__list ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
+      title="Factsheet"
+    >
+      <dt className="ons-metadata__term ons-grid__col ons-col-3@m">CaseId:</dt>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.CaseId}</dd>
+      <dt className="ons-metadata__term ons-grid__col ons-col-3@m">Address:</dt>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.Address.AddressLine1}</dd>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.Address.AddressLine2}</dd>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.Address.AddressLine3}</dd>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.Address.AddressLine4}</dd>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.Address.Town}</dd>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.Address.County}</dd>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.Address.Postcode}</dd>
+      <dt className="ons-metadata__term ons-grid__col ons-col-3@m">Interviewer:</dt>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.InterviewerName}</dd>
+      <dt className="ons-metadata__term ons-grid__col ons-col-3@m">Outcome code:</dt>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.OutcomeCode}</dd>
+      <dt className="ons-metadata__term ons-grid__col ons-col-3@m">Household size:</dt>
+      <dd className="ons-metadata__value ons-grid__col ons-col-9@m">{factsheet.NumberOfRespondents}</dd>
+      <dt className="ons-metadata__term ons-grid__col ons-col-3@m">Respondents:</dt>
       {factsheet.Respondents.map((respondent) => (
-        <div key={respondent.RespondentName}>
+        <dd className="ons-metadata__value ons-grid__col ons-col-9@m">
           {respondent.RespondentName}
+          {' '}
           {String(respondent.DateOfBirth)}
-        </div>
+        </dd>
       ))}
-    </div>
+    </dl>
   );
 }
