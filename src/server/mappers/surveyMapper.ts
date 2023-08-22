97cceabdf644f9a305a0de1f546239ebcfeea76f
westjake <jacob.west@ons.gov.uk>
2023-08-22 12:33:16 +0100

    added survey mapper test

--- src/server/mappers/surveyMapper.ts
+++ src/server/mappers/surveyMapper.ts
@@ -2,6 +2,24 @@ import { Questionnaire } from 'blaise-api-node-client';
 import { Survey } from '../../common/interfaces/surveyInterface';
 
 export default function mapSurveys(questionnaires: Questionnaire[]): Survey[] {
-  console.debug(questionnaires);
-  return [];
+  let surveys: Survey[] = []; 
+  let surveyIndex = 0;
+
+  questionnaires.forEach((questionaire) => {
+      let surveyName = questionaire.name.slice(0, 3);
+      let existingIndex = surveys.findIndex(s => s.name === surveyName);
+      if(existingIndex === -1) {
+        surveys[surveyIndex] = {
+          name: surveyName,
+          questionnaires: [questionaire]
+        };
+
+        surveyIndex++;
+      }
+      else {
+        surveys[existingIndex]?.questionnaires.push(questionaire);
+      }
+  });
+
+  return surveys;
 }
