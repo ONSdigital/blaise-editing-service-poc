107fe2bfe0367b9f80bfb325f945c40edcfee5d1
westjake <jacob.west@ons.gov.uk>
2023-08-23 17:07:40 +0100

    Added questionnaire list to surveys page and style

--- src/client/components/QuestionnairesList.tsx
+++ src/client/components/QuestionnairesList.tsx
@@ -1,35 +1,60 @@
 import { Questionnaire } from 'blaise-api-node-client';
-import { ONSPanel, ONSTable } from 'blaise-design-system-react-components';
+import { ONSPanel } from 'blaise-design-system-react-components';
+import { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 
 interface QuestionnairesListProps {
   questionnaires: Questionnaire[];
 }
 
-export default function QuestionnairesList({ questionnaires }: QuestionnairesListProps) {
+const tableStyle = {
+  margin: '15px',
+  width: '700px',
+};
+
+const linkStyle = {
+  textDecoration: 'none',
+  color: 'blue',
+};
+
+export default function QuestionnairesList({ questionnaires }: QuestionnairesListProps): ReactElement {
   if (questionnaires.length === 0) {
     return (
       <ONSPanel spacious status="info">There are no questionnaires available</ONSPanel>
     );
   }
   return (
-    <ONSTable
-      tableID="questionnaire-table"
-      columns={[
-        'Questionnaire name',
-        'Server park',
-        'Number of cases',
-      ]}
-    >
-      <>
-        {questionnaires.map((questionnaire) => (
-          <tr key={questionnaire.name} className="ons-table__row" data-testid="questionnaire-table-row">
-            <td className="ons-table__cell"><Link to={`/questionnaires/${questionnaire.name}/cases`}>{questionnaire.name}</Link></td>
-            <td className="ons-table__cell">{questionnaire.serverParkName}</td>
-            <td className="ons-table__cell">{questionnaire.dataRecordCount}</td>
-          </tr>
-        ))}
-      </>
-    </ONSTable>
+    <table style={tableStyle} id="questionnaire-table">
+      <thead>
+        <th style={{
+          width: '50%', textAlign: 'left', margin: '5px', padding: '5px',
+        }}
+        >
+          <strong>Questionnaire name</strong>
+        </th>
+        <th style={{
+          width: '50%', textAlign: 'left', margin: '5px', padding: '5px',
+        }}
+        >
+          <strong>Case count</strong>
+        </th>
+      </thead>
+      {questionnaires.map((questionnaire) => (
+        <tr key={questionnaire.name} data-testid="questionnaire-table-row">
+          <td style={{
+            width: '50%', textAlign: 'left', margin: '5px', padding: '5px',
+          }}
+          >
+            <Link to={`/questionnaires/${questionnaire.name}/cases`} style={linkStyle}>{questionnaire.name}</Link>
+          </td>
+          <td style={{
+            width: '50%', textAlign: 'left', margin: '5px', padding: '5px',
+          }}
+          >
+            {questionnaire.dataRecordCount}
+          </td>
+        </tr>
+      ))}
+    </table>
   );
 }
--- src/client/components/SurveysList.tsx
+++ src/client/components/SurveysList.tsx
@@ -1,20 +1,22 @@
+import { Accordion } from 'blaise-design-system-react-components';
+import { ExpandableContent } from 'blaise-design-system-react-components/build/src/components/Accordion';
+import { ReactElement } from 'react';
 import { Survey } from '../../common/interfaces/surveyInterface';
+import QuestionnairesList from './QuestionnairesList';
 
 interface SurveysListProps {
   surveys: Survey[]
 }
 
-export default function SurveysList({ surveys }: SurveysListProps) {
+function CreateContent(surveys:Survey[]):ExpandableContent[] {
+  return surveys.map(({ name, questionnaires }) => ({ title: name, content: <QuestionnairesList questionnaires={questionnaires} /> }));
+}
+
+export default function SurveysList({ surveys }: SurveysListProps): ReactElement {
   return (
     <>
-      {surveys.map((survey) => (
-        survey.questionnaires.map((questionnaire) => (
-          <div>
-            <div className="ons-table__cell">{questionnaire.serverParkName}</div>
-            <div className="ons-table__cell">{questionnaire.dataRecordCount}</div>
-          </div>
-        ))))}
-      ;
+      <br />
+      <Accordion ShowAllEnabled Expandables={CreateContent(surveys)} />
     </>
   );
 }
