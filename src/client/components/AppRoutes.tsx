34d58ee1abae5ccf2597c1868de30e4d821f1f4d
TKeable <toby.keable@ons.gov.uk>
2023-08-31 14:46:28 +0100

    Fixed home and refresh issue

--- src/client/components/AppRoutes.tsx
+++ src/client/components/AppRoutes.tsx
@@ -13,8 +13,17 @@ export default function AppRoutes({ loginClient }: RoutesProps): ReactElement {
   const [userRole, setUserRole] = useState<string>();
 
   useEffect(() => {
+    console.debug('Enter use effect');
+
+    loginClient.authManager.loggedIn().then((isLoggedIn: boolean) => {
+      console.debug('Enter authManager Log in', isLoggedIn);
+      loginClient.setLoggedIn(isLoggedIn);
+    });
+
     if (loginClient.loggedIn) {
+      console.debug('Enter loginClient.loggedIn');
       loginClient.getRoleOfCurrentUser().then((role) => {
+        console.debug('Enter getRole');
         setUserRole(role);
       });
     }
--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { ONSPanel } from 'blaise-design-system-react-components';
 import { AuthManager, LoginForm } from 'blaise-login-react-client';
-import { ReactElement, useEffect } from 'react';
+import { ReactElement } from 'react';
 
 interface LoginProps {
   authManager:AuthManager;
@@ -8,12 +8,6 @@ interface LoginProps {
 }
 
 export default function LoginPage({ authManager, setLoggedIn }: LoginProps): ReactElement {
-  useEffect(() => {
-    authManager.loggedIn().then((isLoggedIn: boolean) => {
-      setLoggedIn(isLoggedIn);
-    });
-  }, [authManager, setLoggedIn]);
-
   return (
     <>
       <ONSPanel status="info">Enter your Blaise username and password</ONSPanel>
