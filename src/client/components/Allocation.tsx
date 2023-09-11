82627389ad363db8ba974945e87ee3591ac3f0b0
jamie kerr <jamiestephenkerr@gmail.com>
2023-09-11 13:22:56 +0100

    added HC users on allocation page

---
+++ src/client/components/Allocation.tsx
@@ -0,0 +1,21 @@
+import { User } from "blaise-api-node-client";
+
+const caseEditors:User[] = [{
+    name: 'Toby Maguire',
+    role: 'Editor',
+    serverParks: ['gusty'],
+    defaultServerPark: 'gusty',
+  },
+  {
+    name: 'Richmond Ricecake',
+    role: 'Editor',
+    serverParks: ['gusty'],
+    defaultServerPark: 'gusty',
+  },
+  {
+    name: 'Sarah Bosslady',
+    role: 'Editor',
+    serverParks: ['gusty'],
+    defaultServerPark: 'gusty',
+  }
+];
