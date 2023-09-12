854f3700085615024d3bfbe983911482893bad0f
jamie kerr <jamiestephenkerr@gmail.com>
2023-09-12 21:50:55 +0100

    tidied up allocation and fixed issue with login

--- src/client/pages/Allocation.tsx
+++ src/client/pages/Allocation.tsx
@@ -1,21 +1,23 @@
 import { User } from 'blaise-api-node-client';
-import { ONSPanel, ONSSelect } from 'blaise-design-system-react-components';
+import {
+  ONSButton, ONSPanel, ONSSelect, ONSTextInput,
+} from 'blaise-design-system-react-components';
 import { useParams } from 'react-router-dom';
 
 const caseEditors:User[] = [{
-  name: 'Toby Maguire',
+  name: 'Toby Maguire (0 cases)',
   role: 'Editor',
   serverParks: ['gusty'],
   defaultServerPark: 'gusty',
 },
 {
-  name: 'Richmond Ricecake',
+  name: 'Richmond Ricecake (20 cases)',
   role: 'Editor',
   serverParks: ['gusty'],
   defaultServerPark: 'gusty',
 },
 {
-  name: 'Sarah Bosslady',
+  name: 'Sarah Bosslady (0 cases)',
   role: 'Editor',
   serverParks: ['gusty'],
   defaultServerPark: 'gusty',
@@ -33,14 +35,27 @@ function displayAllocation(questionnaireName:string) {
   return (
     <div>
       <ONSPanel spacious status="info">
-        Allocations for
+        There are 20 cases left to allocate for
+        {' '}
         {questionnaireName}
       </ONSPanel>
+
       <ONSSelect
         value="Select the editor"
         id="select-editor"
         label="Select editor"
         options={mapEditorsToOptionList(caseEditors)}
+      />
+
+      <ONSTextInput
+        id="select-number"
+        label="Select number of cases to allocate"
+        placeholder="50"
+      />
+
+      <ONSButton
+        label="Allocate"
+        primary
       />
     </div>
   );
