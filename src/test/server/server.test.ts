9fbc55bc093fdf2b35cbdad433bd2357157597d4
jamie kerr <jamiestephenkerr@gmail.com>
2023-09-22 15:45:45 +0100

    Added test to cover default react rendering

--- src/test/server/server.test.ts
+++ src/test/server/server.test.ts
@@ -1,9 +1,13 @@
+/*
+* @jest-environment node
+*/
 import listEndpoints, { Endpoint } from 'express-list-endpoints';
 import { IMock, Mock } from 'typemoq';
 import NodeServer from '../../server/server';
 import FakeConfigurationProvider from './configuration/FakeConfigurationProvider';
 import BlaiseApi from '../../server/api/BlaiseApi';
-
+import supertest from 'supertest';
+import path from 'path';
 // create fake config
 const configFake = new FakeConfigurationProvider('restapi.blaise.com', 'dist', 5000, 'gusty', 'cati.blaise.com', 'richlikesricecakes', '12h', ['DST']);
 
@@ -11,7 +15,7 @@ const configFake = new FakeConfigurationProvider('restapi.blaise.com', 'dist', 5
 const blaiseApiMock: IMock<BlaiseApi> = Mock.ofType(BlaiseApi);
 
 // create service to test
-const sut = NodeServer(configFake, blaiseApiMock.object);
+const server = NodeServer(configFake, blaiseApiMock.object);
 
 describe('All expected routes are registered', () => {
   it('should contain expected routes', async () => {
@@ -31,9 +35,26 @@ describe('All expected routes are registered', () => {
     ];
 
     // act
-    const actualEndpoints = listEndpoints(sut);
+    const actualEndpoints = listEndpoints(server);
 
     // assert
     expect(actualEndpoints).toEqual(expectedEndpoints);
+  });
+});
+
+describe('Render react pages as default route', () => {
+  it('should render the home page', async () => {
+    // arrange
+    server.set('views', path.join(__dirname, '../../../build'));
+    const sut = supertest(server);
+   
+    // act   
+   const result = await sut.get('/');
+    
+    // assert
+    expect(result.error).toBeFalsy();
+    expect(result.statusCode).toEqual(200);
+    expect(result.type).toEqual('text/html');
+    expect(result.text).toContain('Web site created using create-react-app');   
   });
 });
