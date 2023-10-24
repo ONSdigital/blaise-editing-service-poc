ce5b8db699fb8dd1c33c3697ebc6d0fcb8041d2e
jamie kerr <jamiestephenkerr@gmail.com>
2023-10-24 12:11:51 +0100

    Add starter conditional route for 10

--- src/client/components/AppContent.tsx
+++ src/client/components/AppContent.tsx
@@ -6,6 +6,7 @@ import Cases from '../pages/Cases';
 import CaseFactsheet from '../pages/CaseFactsheet';
 import Allocate from '../pages/Allocate';
 import AllocatedList from '../pages/CurrentlyAllocated';
+import ConditionalRoute from './ConditionalRoute';
 
 interface AppContentProps {
   user:User
@@ -14,11 +15,18 @@ interface AppContentProps {
 export default function AppContent({ user }: AppContentProps): ReactElement {
   return (
     <Routes>
-      <Route path="/" element={<Surveys user={user} />} />
-      <Route path="questionnaires/:questionnaireName/cases/" element={<Cases user={user} />} />
-      <Route path="questionnaires/:questionnaireName/cases/:caseId/factsheet" element={<CaseFactsheet />} />
-      <Route path="questionnaires/:questionnaireName/allocation/allocate" element={<Allocate />} />
-      <Route path="questionnaires/:questionnaireName/allocation/allocated" element={<AllocatedList />} />
+      <Route element={<ConditionalRoute condition={user.role === 'Manager'}/>}>
+        <Route path="/" element={<Surveys user={user} />} />
+        <Route path="questionnaires/:questionnaireName/allocation/allocate" element={<Allocate />} />
+      </Route>
+
+      <Route element={<ConditionalRoute condition={user.role === 'Editor'}/>}>
+        <Route path="/" element={<Surveys user={user} />} />
+        <Route path="questionnaires/:questionnaireName/cases/" element={<Cases user={user} />} />
+        <Route path="questionnaires/:questionnaireName/cases/:caseId/factsheet" element={<CaseFactsheet />} />
+        <Route path="questionnaires/:questionnaireName/allocation/allocated" element={<AllocatedList />} />
+      </Route>
+
     </Routes>
   );
 }
