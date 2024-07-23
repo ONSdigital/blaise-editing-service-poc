76ea3ff0fac13573adead7beb244ffe8821a029c
jamie kerr <jamiestephenkerr@gmail.com>
2024-07-23 11:00:24 +0100

    Updated questionnaire details

--- src/client/Supervisor/Components/SupervisorsQuestionnaireDetails.tsx
+++ src/client/Supervisor/Components/SupervisorsQuestionnaireDetails.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 import { QuestionnaireDetails } from '../../../common/interfaces/surveyInterface';
 
 interface SupervisorsQuestionnairesDetailsProps {
@@ -11,7 +12,6 @@ export default function SupervisorsQuestionnaireDetails({ questionnaire }: Super
       margin: '0 0 0 2px', padding: '10px', backgroundColor: '#e7f3ec', border: '1px dotted',
     }}
     >
-
       <dl
         className="ons-description-list ons-description-list__items ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
         title="Information about this business and its survey requirements"
@@ -23,12 +23,13 @@ export default function SupervisorsQuestionnaireDetails({ questionnaire }: Super
         <dd className="ons-description-list__value ons-grid__col ons-col-7@m"><strong>1000</strong></dd>
         <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Total number of cases allocated:</dt>
         <dd className="ons-description-list__value ons-grid__col ons-col-7@m"><strong>30</strong></dd>
+        <dd className="ons-description-list__value ons-grid__col ons-col-7@m"><Link to="/">Allocate</Link></dd>
 
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Doctor Doom:</dt>
+        <dt className="ons-description-list__term ons-grid__col ons-col-5@m"><Link to="/">Doctor Doom:</Link></dt>
         <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
           <strong>10</strong>
           {' '}
-          case(s) assigned
+          case(s) allocated
         </dd>
         <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
           <strong>5</strong>
@@ -51,60 +52,28 @@ export default function SupervisorsQuestionnaireDetails({ questionnaire }: Super
           case(s) with queries
         </dd>
 
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Captain Fantastic:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>10</strong>
-          {' '}
-          case(s) assigned
-        </dd>
+        <dt className="ons-description-list__term ons-grid__col ons-col-5@m"><Link to="/">Captain Fantastic:</Link></dt>
         <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
           <strong>5</strong>
+          {' '}
+          case(s) allocated
+        </dd>
+        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
+          <strong>4</strong>
           {' '}
           case(s) not started
         </dd>
         <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>2</strong>
+          <strong>1</strong>
           {' '}
           case(s) in progress
         </dd>
+        <dt className="ons-description-list__term ons-grid__col ons-col-5@m"><Link to="/">The Thing:</Link></dt>
         <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>2</strong>
+          <strong>0</strong>
           {' '}
-          case(s) completed
+          case(s) allocated
         </dd>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>1</strong>
-          {' '}
-          case(s) with queries
-        </dd>
-
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">The Thing:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>10</strong>
-          {' '}
-          case(s) assigned
-        </dd>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>5</strong>
-          {' '}
-          case(s) not started
-        </dd>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>2</strong>
-          {' '}
-          case(s) in progress
-        </dd>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>2</strong>
-          {' '}
-          case(s) completed
-        </dd>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">
-          <strong>1</strong>
-          {' '}
-          case(s) with queries
-        </dd>
-
       </dl>
     </div>
   );
