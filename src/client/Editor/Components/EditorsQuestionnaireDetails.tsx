d2ed4aed72aff4b530b9fb2bcc195eeb9569e146
Jake West <jacob.west@ons.gov.uk>
2024-07-25 13:59:39 +0100

    changed round onsPannel

--- src/client/Editor/Components/EditorsQuestionnaireDetails.tsx
+++ src/client/Editor/Components/EditorsQuestionnaireDetails.tsx
@@ -10,18 +10,20 @@ interface EditorsQuestionnairesDetailsProps {
 export default function EditorsQuestionnairesDetails({ questionnaire }: EditorsQuestionnairesDetailsProps): ReactElement {
   return (
     <div className="questionnaire">
-      <dl
-        className="ons-metadata ons-metadata__list ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
-        title="Questionnares"
-        data-testid="QuestionnaireList"
-        style={{ padding: '0 0 15px 5px' }}
-      >
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Field period:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m"><strong>{questionnaire.fieldPeriod}</strong></dd>
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Cases assigned to me:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m"><strong>3</strong></dd>
+      <ONSPanel status="info">
+        <dl
+          className="ons-metadata ons-metadata__list ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
+          title="Questionnares"
+          data-testid="QuestionnaireList"
+          style={{ padding: '0 0 15px 5px' }}
+        >
+          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Field period:</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-7@m"><strong>{questionnaire.fieldPeriod}</strong></dd>
+          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Cases assigned to me:</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-7@m"><strong>3</strong></dd>
 
-      </dl>
+        </dl>
+      </ONSPanel>
       <ONSSelect
         defaultValue="in-progress"
         id="filter-cases"
@@ -43,68 +45,68 @@ export default function EditorsQuestionnairesDetails({ questionnaire }: EditorsQ
         ]}
         value=""
       />
-      <ONSPanel status="info">
-        <ONSTable
-          columns={[
-            'Case ID',
-            'Status',
-            '',
-          ]}
-        >
-          <>
-            <tr
-              className="ons-table__row"
-              data-testid="questionnaire-table-row"
-            >
-              <td className="ons-table__cell">
-                10001011
-              </td>
-              <td className="ons-table__cell status">
-                In Progress
-              </td>
-              <td className="ons-table__cell links">
-                <Link className="Edit" to="/summary">Summary</Link>
-                {' | '}
-                <Link className="Edit" to="/">Edit</Link>
-              </td>
-            </tr>
 
-            <tr
-              className="ons-table__row"
-              data-testid="questionnaire-table-row2"
-            >
-              <td className="ons-table__cell">
-                10001012
-              </td>
-              <td className="ons-table__cell status">
-                Complete
-              </td>
-              <td className="ons-table__cell links">
-                <Link className="Edit" to="/">Summary</Link>
-                {' | '}
-                <Link className="Edit" to="/">Edit</Link>
-              </td>
-            </tr>
+      <ONSTable
+        columns={[
+          'Case ID',
+          'Status',
+          '',
+        ]}
+      >
+        <>
+          <tr
+            className="ons-table__row"
+            data-testid="questionnaire-table-row"
+          >
+            <td className="ons-table__cell">
+              10001011
+            </td>
+            <td className="ons-table__cell status">
+              In Progress
+            </td>
+            <td className="ons-table__cell links">
+              <Link className="Edit" to="/summary">Summary</Link>
+              {' | '}
+              <Link className="Edit" to="/">Edit</Link>
+            </td>
+          </tr>
 
-            <tr
-              className="ons-table__row"
-              data-testid="questionnaire-table-row3"
-            >
-              <td className="ons-table__cell">
-                10001013
-              </td>
-              <td className="ons-table__cell status">
-                Not Started
-              </td>
-              <td className="ons-table__cell links">
-                <Link className="Edit" to="/">Summary</Link>
-                {' | '}
-                <Link className="Edit" to="/">Edit</Link>
-              </td>
-            </tr>
-          </>
-        </ONSTable>
-      </ONSPanel>
+          <tr
+            className="ons-table__row"
+            data-testid="questionnaire-table-row2"
+          >
+            <td className="ons-table__cell">
+              10001012
+            </td>
+            <td className="ons-table__cell status">
+              Complete
+            </td>
+            <td className="ons-table__cell links">
+              <Link className="Edit" to="/">Summary</Link>
+              {' | '}
+              <Link className="Edit" to="/">Edit</Link>
+            </td>
+          </tr>
+
+          <tr
+            className="ons-table__row"
+            data-testid="questionnaire-table-row3"
+          >
+            <td className="ons-table__cell">
+              10001013
+            </td>
+            <td className="ons-table__cell status">
+              Not Started
+            </td>
+            <td className="ons-table__cell links">
+              <Link className="Edit" to="/">Summary</Link>
+              {' | '}
+              <Link className="Edit" to="/">Edit</Link>
+            </td>
+          </tr>
+        </>
+      </ONSTable>
+
     </div>
   );
 }
--- src/client/Supervisor/Components/SupervisorsQuestionnaireDetails.tsx
+++ src/client/Supervisor/Components/SupervisorsQuestionnaireDetails.tsx
@@ -10,104 +10,105 @@ interface SupervisorsQuestionnairesDetailsProps {
 export default function SupervisorsQuestionnaireDetails({ questionnaire }: SupervisorsQuestionnairesDetailsProps): ReactElement {
   return (
     <div className="questionnaire">
-      <dl
-        className="ons-metadata ons-metadata__list ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
-        title="Questionnares"
-        data-testid="QuestionnaireList"
-        style={{ padding: '0 0 15px 5px' }}
-      >
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Field period:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">{questionnaire.fieldPeriod}</dd>
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Total number of cases:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">1000</dd>
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Number of cases not allocated:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">970</dd>
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Number of cases allocated:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">25</dd>
-        <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Number of cases completed:</dt>
-        <dd className="ons-description-list__value ons-grid__col ons-col-7@m">9</dd>
-      </dl>
-
       <ONSPanel status="info">
-        <ONSTable
-          columns={[
-            'Editor',
-            'Allocated',
-            'Completed',
-            'Queried',
-            '',
-          ]}
+        <dl
+          className="ons-metadata ons-metadata__list ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
+          title="Questionnares"
+          data-testid="QuestionnaireList"
+          style={{ padding: '0 0 15px 5px' }}
         >
-          <>
-            <tr
-              className="ons-table__row"
-              data-testid="questionnaire-table-row"
-            >
-              <td className="ons-table__cell">
-                <Link to="/">Doctor Doom:</Link>
-              </td>
-              <td className="ons-table__cell status">
-                10
-              </td>
-              <td className="ons-table__cell ">
-                5
-              </td>
-              <td className="ons-table__cell ">
-                0
-              </td>
-              <td className="ons-table__cell links">
-                <Link to="/allocate">Allocate</Link>
-                {' | '}
-                <Link to="/reallocate">Reallocate</Link>
-              </td>
-            </tr>
-            <tr
-              className="ons-table__row"
-              data-testid="questionnaire-table-row"
-            >
-              <td className="ons-table__cell">
-                <Link to="/">Captain Fantastic:</Link>
-              </td>
-              <td className="ons-table__cell status">
-                5
-              </td>
-              <td className="ons-table__cell ">
-                4
-              </td>
-              <td className="ons-table__cell ">
-                1
-              </td>
-              <td className="ons-table__cell links">
-                <Link to="/allocate">Allocate</Link>
-                {' | '}
-                <Link to="/reallocate">Reallocate</Link>
-              </td>
-            </tr>
-            <tr
-              className="ons-table__row"
-              data-testid="questionnaire-table-row"
-            >
-              <td className="ons-table__cell">
-                <Link to="/">The thing:</Link>
-              </td>
-              <td className="ons-table__cell status">
-                0
-              </td>
-              <td className="ons-table__cell ">
-                0
-              </td>
-              <td className="ons-table__cell ">
-                0
-              </td>
-              <td className="ons-table__cell links">
-                <Link to="/allocate">Allocate</Link>
-                {' | '}
-                <Link to="/reallocate">Reallocate</Link>
-              </td>
-            </tr>
-          </>
-        </ONSTable>
+          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Field period:</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">{questionnaire.fieldPeriod}</dd>
+          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Total number of cases:</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">1000</dd>
+          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Number of cases not allocated:</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">970</dd>
+          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Number of cases allocated:</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">25</dd>
+          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Number of cases completed:</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">9</dd>
+        </dl>
       </ONSPanel>
+
+      <ONSTable
+        columns={[
+          'Editor',
+          'Allocated',
+          'Completed',
+          'Queried',
+          '',
+        ]}
+      >
+        <>
+          <tr
+            className="ons-table__row"
+            data-testid="questionnaire-table-row"
+          >
+            <td className="ons-table__cell">
+              <Link to="/">Doctor Doom:</Link>
+            </td>
+            <td className="ons-table__cell status">
+              10
+            </td>
+            <td className="ons-table__cell ">
+              5
+            </td>
+            <td className="ons-table__cell ">
+              0
+            </td>
+            <td className="ons-table__cell links">
+              <Link to="/allocate">Allocate</Link>
+              {' | '}
+              <Link to="/reallocate">Reallocate</Link>
+            </td>
+          </tr>
+          <tr
+            className="ons-table__row"
+            data-testid="questionnaire-table-row"
+          >
+            <td className="ons-table__cell">
+              <Link to="/">Captain Fantastic:</Link>
+            </td>
+            <td className="ons-table__cell status">
+              5
+            </td>
+            <td className="ons-table__cell ">
+              4
+            </td>
+            <td className="ons-table__cell ">
+              1
+            </td>
+            <td className="ons-table__cell links">
+              <Link to="/allocate">Allocate</Link>
+              {' | '}
+              <Link to="/reallocate">Reallocate</Link>
+            </td>
+          </tr>
+          <tr
+            className="ons-table__row"
+            data-testid="questionnaire-table-row"
+          >
+            <td className="ons-table__cell">
+              <Link to="/">The thing:</Link>
+            </td>
+            <td className="ons-table__cell status">
+              0
+            </td>
+            <td className="ons-table__cell ">
+              0
+            </td>
+            <td className="ons-table__cell ">
+              0
+            </td>
+            <td className="ons-table__cell links">
+              <Link to="/allocate">Allocate</Link>
+              {' | '}
+              <Link to="/reallocate">Reallocate</Link>
+            </td>
+          </tr>
+        </>
+      </ONSTable>
+
     </div>
   );
 }
