547518ef92a2f24470e00f52526757c6eb7f70fd
jamie kerr <jamiestephenkerr@gmail.com>
2024-08-15 12:20:08 +0100

    Refactored

--- src/client/api/NodeApi.ts
+++ src/client/api/NodeApi.ts
@@ -4,9 +4,10 @@ import { CaseEditInformation } from 'blaise-api-node-client/lib/cjs/interfaces/c
 import notFound from '../../server/helpers/axiosHelper';
 import { Survey } from '../../common/interfaces/surveyInterface';
 
-import { SupervisorInformation } from '../../common/interfaces/supervisorInterface';
+import { SupervisorInformation } from '../Interfaces/supervisorInterface';
 import { EditorInformation } from '../Interfaces/editorInterface';
 import mapEditorInformation from '../Mappers/editorInformaitionMapper';
+import mapSupervisorInformation from '../Mappers/supervisorInformationMapper';
 
 async function getDataFromNode<T>(url: string, notFoundError: string): Promise<T> {
   try {
@@ -25,9 +26,13 @@ export async function getSurveys(): Promise<Survey[]> {
   return getDataFromNode('/api/surveys', 'Unable to find surveys, please contact Richmond Rice');
 }
 
+async function getCaseEditInformation(questionnaireName: string) {
+  return getDataFromNode<CaseEditInformation[]>(`/api/${questionnaireName.toUpperCase()}/cases/edit`, 'Unable to find case edit information, please contact Richmond Rice');
+}
+
 export async function getEditorInformation(questionnaireName: string, editorUsername:string): Promise<EditorInformation> {
   // TODO Fix the URL upper
-  const caseEditInformationList = await getDataFromNode<CaseEditInformation[]>(`/api/${questionnaireName.toUpperCase()}/cases/edit`, 'Unable to find case edit information, please contact Richmond Rice');
+  const caseEditInformationList = await getCaseEditInformation(questionnaireName);
   const caseEditInformationListForEditor = caseEditInformationList.filter((caseEditInformation) => caseEditInformation.assignedTo === editorUsername);
 
   return mapEditorInformation(caseEditInformationListForEditor);
@@ -35,5 +40,7 @@ export async function getEditorInformation(questionnaireName: string, editorUser
 
 export async function getSupervisorEditorInformation(questionnaireName: string): Promise<SupervisorInformation> {
   // TODO Fix the URL upper
-  return getDataFromNode(`/api/${questionnaireName.toUpperCase()}/supervisor/edit`, 'Unable to find supervisor information, please contact Richmond Rice');
+  const caseEditInformationList = await getCaseEditInformation(questionnaireName);
+
+  return mapSupervisorInformation(caseEditInformationList);
 }
