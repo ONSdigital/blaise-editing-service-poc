d698e60f6d358cd34cd392f8267a75adc42bf2cf
jamie kerr <jamiestephenkerr@gmail.com>
2024-09-26 09:35:37 +0100

    Updated nav

--- src/client/Common/components/LayoutTemplate.tsx
+++ src/client/Common/components/LayoutTemplate.tsx
@@ -3,6 +3,7 @@ import {
 } from 'blaise-design-system-react-components';
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import SubNavigationTemplate from './SubNavigation';
 
 const divStyle = {
   minHeight: 'calc(67vh)',
@@ -17,14 +18,6 @@ interface LayoutTemplateProps {
 export default function LayoutTemplate({ children, showSignOutButton, signOut }: LayoutTemplateProps) {
   const navigate = useNavigate();
 
-  /*   const navigationLinks = [
-    {
-      endpoint: '/',
-      id: 'Home',
-      label: 'Home',
-    },
-  ]; */
-
   return (
 
     <>
@@ -35,18 +28,23 @@ export default function LayoutTemplate({ children, showSignOutButton, signOut }:
         signOutButton={showSignOutButton}
         signOutFunction={() => { signOut(); navigate('/'); }}
       />
+
+      <SubNavigationTemplate>
+        <Link
+          className="ons-navigation__link"
+          to=".."
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(-1);
+          }}
+        >
+          <strong>{'< Back'}</strong>
+        </Link>
+      </SubNavigationTemplate>
+
       <DefaultErrorBoundary>
         <div style={divStyle} className="ons-page__container ons-container" data-testid="app-content">
-          <Link
-            className="nav"
-            to=".."
-            onClick={(e) => {
-              e.preventDefault();
-              navigate(-1);
-            }}
-          >
-            {'< back'}
-          </Link>
+
           {children}
         </div>
       </DefaultErrorBoundary>
