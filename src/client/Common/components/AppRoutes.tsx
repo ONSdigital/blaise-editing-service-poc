755d9be2af67daccfabfe74e5f6f9dd93223b0df
Elinor <46898565+elthorne@users.noreply.github.com>
2024-11-07 15:06:57 +0000

    fix: wrap <Route> in <Routes>
    refs: BLAIS5-4391

--- src/client/Common/components/AppRoutes.tsx
+++ src/client/Common/components/AppRoutes.tsx
@@ -1,9 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
 import { ReactElement } from 'react';
-import { Route } from 'react-router-dom';
 import SupervisorHome from '../../Supervisor/Pages/SupervisorHome';
 
 export default function AppContent(): ReactElement {
   return (
-    <Route path="/" element={<SupervisorHome />} />
+    <Routes>
+      <Route path="/" element={<SupervisorHome />} />
+    </Routes>
   );
 }
