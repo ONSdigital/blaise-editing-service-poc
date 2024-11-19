78cd54a77f401608a50d4e7a8df0aa7120610525
jamie kerr <jamiestephenkerr@gmail.com>
2024-11-19 08:54:41 +0000

    Upedated list to add additional options

--- src/client/Supervisor/Components/AllocateContentForm.tsx
+++ src/client/Supervisor/Components/AllocateContentForm.tsx
@@ -85,8 +85,32 @@ export default function AllocateCases({
           value: '50',
         },
         {
+          label: '45',
+          value: '45',
+        },
+        {
+          label: '40',
+          value: '40',
+        },
+        {
+          label: '35',
+          value: '35',
+        },
+        {
+          label: '30',
+          value: '30',
+        },
+        {
           label: '25',
           value: '25',
+        },
+        {
+          label: '20',
+          value: '20',
+        },
+        {
+          label: '15',
+          value: '15',
         },
         {
           label: '10',
